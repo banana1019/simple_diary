7baa0eae5eeb76d6375734fc2154be86b625a9a0
banana1019 <jyho930@gmail.com>
2022-09-19 17:56:02 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
-function App() {
+const dummyList = [
+  {
+    id: 1,
+    author: "나나",
+    content: "하이1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "치치",
+    content: "하이2",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "죠죠",
+    content: "하이3",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+];
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
-}
+};
 
 export default App;
